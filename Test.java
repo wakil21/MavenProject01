786c783dcb31f063fdd622deeec090451ebab293
wakil21 <wakilkumar111@hotmail.com>
2019-06-23 14:07:37 +0530

    final commit

---
+++ Test.java
@@ -0,0 +1,13 @@
+class Test
+{
+
+public static void main(String[] args)
+  {
+
+System.out.println("hello world");
+System.out.println("hello world");
+System.out.println("hello world");
+https://github.com/wakil21/MavenProject01.git
+
+  }
+}
